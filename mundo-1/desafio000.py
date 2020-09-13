49a7eb42a85c218a6d22a4e8a76c5c01e04bde68
Fernando Angelo <nandoangelo@gmail.com>
2020-09-12 22:18:26 -0300

    Create desafio000.py
    Primeiro desafio no curso de Python.

---
+++ mundo-1/desafio000.py
@@ -0,0 +1,18 @@
+# DESAFIO 000 
+# Aqui eu resolvi experimentar o uso das variáveis em Python
+#
+# -------------------------------------- Mundo I / Aula 04 ----
+
+# Entradas
+nome = input ('Qual o seu nome?\n')
+idade = input ('Quantos anos você tem?\n')
+
+# Saida
+print('Olá,', nome,'. Com ', idade, 'anos eu também já programava.')
+
+'''
+ Usando entrada de dados no Python 3 e montando a saída usando 
+ vírgulas. Detalhe que quando usamos vírgula o interpretador 
+ adiciona espaços na impressão.
+ 
+'''
