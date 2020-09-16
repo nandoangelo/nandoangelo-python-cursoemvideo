92c60a3647a9e8bfff3bdfbb21ff3207e4736ea6
Fernando Angelo <nandoangelo@gmail.com>
2020-09-15 23:18:08 -0300

    Create desafio003.py
    ELE VAI RETORNAR UM ERRO!
    MISSÃO: Crie um script Python que leia dois números e TENTE mostrar a soma entre eles. O objetivo, nessa aula, é fazer com que o programa retorne um erro que será solucionado na aula seguinte.

---
+++ mundo-1/desafio003.py
@@ -0,0 +1,19 @@
+# DESAFIO 003
+# Crie um script Python que leia dois números e TENTE mostrar
+# a soma entre eles.
+#
+# -------------------------------------- Mundo I / Aula 04 ----
+
+num1 = input('Primeiro número: ')
+num2 = input('Segundo número: ')
+
+print('O resultado da soma dos números', num1, 'e', num2,
+      'é:\033[31m', num1 + num2)
+print('TÁ ERRADO, HEIN?!')
+
+'''
+ Aqui usei o código da cor VERMELHA e o deixei habilitado para 
+ as próximas mensagens do programa. Se lembra o porquê desse 
+ código estar errado?
+ 
+'''
