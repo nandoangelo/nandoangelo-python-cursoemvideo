30dd86e151e64c9603af77aabf168b23b5f5dd3f
Fernando Angelo <nandoangelo@gmail.com>
2020-09-15 23:16:09 -0300

    Create desafio002-slim.py
    Uma versão mais simplificada do exercício anterior

---
+++ mundo-1/desafio002-slim.py
@@ -0,0 +1,8 @@
+print('Informe a sua data de nascimento:')
+
+dia = input('Informe o dia = ')
+mes = input('Informe o mês = ')
+ano = input('Informe o ano = ')
+
+# Usando "fStrings"
+print(f'Você nasceu em {dia} do {mes} de {ano}')
